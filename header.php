0767066f0e4cc3bec84d90a9f70d7467f3d23141
Romulo Brasil <me@romulobrasil.com>
2015-04-25 13:02:53 -0300

    Init Projeto

---
+++ header.php
@@ -0,0 +1,16 @@
+     </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+       <header class="header">
+           <nav class="nav-menu">
+               <ul class="menu">
+                   <li class="menu-item"><a href="" class="menu-item-target"></a></li>
+                   <li class="menu-item"><a href="" class="menu-item-target"></a></li>
+                   <li class="menu-item"><a href="" class="menu-item-target"></a></li>
+                   <li class="menu-item"><a href="" class="menu-item-target"></a></li>
+               </ul>
+           </nav>
+       </header>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+include('head.php'); 
+include('header.php'); 
+?>
+   
+    <p>Hello world! This is My Boilerplate.</p>
+
+<?php include('footer.php'); ?>
+
+</body>
+</html>
